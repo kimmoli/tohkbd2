1d4a4cb93ffbc41ea12a114b9b547e6197b5ff0f
kimmoli <kimmo.lindholm@eke.fi>
2014-11-10 23:29:17 +0200

    register KEY_CAPSLOCK

--- src/uinputif.cpp
+++ src/uinputif.cpp
@@ -28,16 +28,17 @@ static const int eventsToRegister[] = { EV_KEY, EV_SW, EV_SYN,
                                         -1 };
 
 static const int keysToRegister[] = {  KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT,KEY_HOME, KEY_END, KEY_PAGEDOWN, KEY_PAGEUP,
-                                        KEY_1, KEY_2, KEY_3, KEY_4, KEY_5, KEY_6, KEY_7, KEY_8, KEY_9, KEY_0,
-                                        KEY_A, KEY_B, KEY_C, KEY_D, KEY_E, KEY_F, KEY_G, KEY_H, KEY_I, KEY_J,
-                                        KEY_K, KEY_L, KEY_M, KEY_N, KEY_O, KEY_P, KEY_Q, KEY_R, KEY_S, KEY_T,
-                                        KEY_U, KEY_V, KEY_W, KEY_X, KEY_Y, KEY_Z, KEY_COMMA, KEY_DOT, KEY_SPACE,
-                                        KEY_LEFTALT, KEY_LEFTSHIFT, KEY_LEFTCTRL,
-                                        KEY_RIGHTALT, KEY_RIGHTSHIFT, KEY_RIGHTCTRL,
-                                        KEY_BACKSPACE, KEY_DELETE, KEY_INSERT, KEY_EQUAL, KEY_MINUS, KEY_TAB,
-                                        KEY_COMPOSE, KEY_APOSTROPHE, KEY_SEMICOLON, KEY_ENTER, KEY_ESC,
-                                        KEY_POWER, KEY_PLAYCD, KEY_PAUSECD, KEY_VOLUMEDOWN, KEY_VOLUMEUP,
-                                        -1 };
+                                       KEY_1, KEY_2, KEY_3, KEY_4, KEY_5, KEY_6, KEY_7, KEY_8, KEY_9, KEY_0,
+                                       KEY_A, KEY_B, KEY_C, KEY_D, KEY_E, KEY_F, KEY_G, KEY_H, KEY_I, KEY_J,
+                                       KEY_K, KEY_L, KEY_M, KEY_N, KEY_O, KEY_P, KEY_Q, KEY_R, KEY_S, KEY_T,
+                                       KEY_U, KEY_V, KEY_W, KEY_X, KEY_Y, KEY_Z, KEY_COMMA, KEY_DOT, KEY_SPACE,
+                                       KEY_LEFTALT, KEY_LEFTSHIFT, KEY_LEFTCTRL,
+                                       KEY_RIGHTALT, KEY_RIGHTSHIFT, KEY_RIGHTCTRL,
+                                       KEY_BACKSPACE, KEY_DELETE, KEY_INSERT, KEY_EQUAL, KEY_MINUS, KEY_TAB,
+                                       KEY_COMPOSE, KEY_APOSTROPHE, KEY_SEMICOLON, KEY_ENTER, KEY_ESC,
+                                       KEY_POWER, KEY_PLAYCD, KEY_PAUSECD, KEY_VOLUMEDOWN, KEY_VOLUMEUP,
+                                       KEY_CAPSLOCK,
+                                       -1 };
 
 static const int switchesToRegister[] = { SW_LID, SW_KEYPAD_SLIDE,
                                           -1 };
