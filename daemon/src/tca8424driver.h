f5a02b6ac248e79487b201ee283d1bfa47e9e0d5
kimmoli <kimmo.lindholm@eke.fi>
2015-02-11 20:48:07 +0200

    [daemon] fix led pin assignments according to revised hw

--- daemon/src/tca8424driver.h
+++ daemon/src/tca8424driver.h
@@ -3,12 +3,14 @@
 #include "driverBase.h"
 
 
-#define LED_CAPSLOCK_ON   0x0101
-#define LED_CAPSLOCK_OFF  0x0100
-#define LED_SYMLOCK_ON    0x0202
-#define LED_SYMLOCK_OFF   0x0200
-#define LED_BACKLIGHT_ON  0x0404
-#define LED_BACKLIGHT_OFF 0x0400
+#define LED_EXTRA_ON      0x0101
+#define LED_EXTRA_OFF     0x0100
+#define LED_CAPSLOCK_ON   0x0202
+#define LED_CAPSLOCK_OFF  0x0200
+#define LED_SYMLOCK_ON    0x0404
+#define LED_SYMLOCK_OFF   0x0400
+#define LED_BACKLIGHT_ON  0x0808
+#define LED_BACKLIGHT_OFF 0x0800
 
 
 class tca8424driver : public DriverBase
