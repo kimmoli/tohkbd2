67faae1edc7b67e2363a62ac0ca37068cf937ba8
Kimmo Lindholm <kimmo.lindholm@eke.fi>
2015-06-28 18:28:06 +0300

    finnish translations

---
+++ settings-ui/i18n/fi.ts
@@ -0,0 +1,247 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="fi" version="2.1">
+<context>
+    <name/>
+    <message id="tohkbd2-settings-app-name">
+        <source>TOHKBD Settings</source>
+        <extracomment>Application name</extracomment>
+        <translation>TOHKBD Asetukset</translation>
+    </message>
+    <message id="about">
+        <source>About...</source>
+        <extracomment>Menu option and header for about page</extracomment>
+        <translation>Tietoja...</translation>
+    </message>
+    <message id="team-header">
+        <source>The TOHKBD2 Team</source>
+        <extracomment>header label for the team presentation</extracomment>
+        <translation>TOHKBD2-tiimi</translation>
+    </message>
+    <message id="version-settings-ui">
+        <source>Settings UI version: </source>
+        <extracomment>Version information field, for Settings UI application version</extracomment>
+        <translation>Asetusten versio: </translation>
+    </message>
+    <message id="version-daemon">
+        <source>Daemon version: </source>
+        <extracomment>Version information field, for daemon version (which runs as root)</extracomment>
+        <translation>Taustaprosessin versio: </translation>
+    </message>
+    <message id="version-user-daemon">
+        <source>User Daemon version: </source>
+        <extracomment>Version information field, for user daemon version (which runs as nemo)</extracomment>
+        <translation>Käyttäjä-taustaprosessin versio: </translation>
+    </message>
+    <message id="version-sailfish">
+        <source>Sailfish version: </source>
+        <extracomment>Version information field, for Sailfish OS version</extracomment>
+        <translation>Sailfish -versio: </translation>
+    </message>
+    <message id="select-application">
+        <source>Select application</source>
+        <extracomment>Application shortcut selector page header</extracomment>
+        <translation>Valitse sovellus</translation>
+    </message>
+    <message id="launch-ext-app">
+        <source>Launching external application...</source>
+        <extracomment>Text shown with busyindicator when starting external application</extracomment>
+        <translation>Käynnistetään sovellusta....</translation>
+    </message>
+    <message id="report-a-bug">
+        <source>Report a bug</source>
+        <extracomment>Main menu selection for FAQ and reporting an issue</extracomment>
+        <translation>Ilmoita ongelmasta</translation>
+    </message>
+    <message id="faq-text">
+        <source>Please first check TOHKBD FAQ page at https://together.jolla.com. Button below opens this link in browser.</source>
+        <extracomment>Description above open faq -button</extracomment>
+        <translation>Katso ensin TOHKBD UKK sivua osoitteessa https://together.jolla.com. Allaoleva painike avaa linkin selaimeen.</translation>
+    </message>
+    <message id="faq-button">
+        <source>Open FAQ</source>
+        <extracomment>Button text, clicking this will open url showing FAQ</extracomment>
+        <translation>Avaa UKK</translation>
+    </message>
+    <message id="report-text">
+        <source>If there is no answer to your issue, please report it. Button below launches email client.</source>
+        <extracomment>Description above report issue -button</extracomment>
+        <translation>Jos et löydä vastausta ongelmaasi UKK:stä, allaolevasta napista voit kertoa ongelmastasi sähköpostilla.</translation>
+    </message>
+    <message id="report-button">
+        <source>Report issue</source>
+        <extracomment>Button text, clicking this will launch email app</extracomment>
+        <translation>Ilmoita ongelmasta</translation>
+    </message>
+    <message id="customize-key-header">
+        <source>Customize %1 -key</source>
+        <extracomment>page header for key customizer, shows label of the key</extracomment>
+        <translation>Muokkaa %1 -näppäintä</translation>
+    </message>
+    <message id="customize-key-warning">
+        <source>You are about to change behaviour of this key</source>
+        <extracomment>warning text shown on customizer page</extracomment>
+        <translation>Olet muuttamassa tämän näppäimen toimintaa</translation>
+    </message>
+    <message id="plain-key">
+        <source>Plain key</source>
+        <extracomment>Text shon above the key without any modifiers, e.g. A</extracomment>
+        <translation>Näppäin</translation>
+    </message>
+    <message id="with-sym-key">
+        <source>Key with Sym</source>
+        <extracomment>Text shon above the key wit sym modifier, e.g. ;</extracomment>
+        <translation>Sym + Näppäin</translation>
+    </message>
+    <message id="none-key">
+        <source>None</source>
+        <extracomment>This text is shown instead of key label (e.g. A) if there is nothing assigned to this key</extracomment>
+        <translation>Ei mitään</translation>
+    </message>
+    <message id="general-settings">
+        <source>General settings</source>
+        <extracomment>Main menu selection for general settings</extracomment>
+        <translation>Yleiset asetukset</translation>
+    </message>
+    <message id="backlight-sect-header">
+        <source>Backlight</source>
+        <extracomment>Section header for backlight settings</extracomment>
+        <translation>Taustavalo</translation>
+    </message>
+    <message id="bg-always-on-sw">
+        <source>Always on</source>
+        <extracomment>Backlight always on switch text</extracomment>
+        <translation>Aina päällä</translation>
+    </message>
+    <message id="bg-always-on-desc">
+        <source>Backlight is always on when keyboard attached and phone&apos;s display is on</source>
+        <extracomment>Backlight always on description</extracomment>
+        <translation>Taustavalo on aina päällä kun näppäimistö on liitetty ja puhelimen näyttö on aktiivinen</translation>
+    </message>
+    <message id="bg-automatic-sw">
+        <source>Automatic</source>
+        <extracomment>Backlight automatic switch text</extracomment>
+        <translation>Automaattinen</translation>
+    </message>
+    <message id="bg-automatic-desc">
+        <source>Automatic backlight enable or always off</source>
+        <extracomment>Backlight automatic description</extracomment>
+        <translation>Taustavalo kytkeytyy päälle säädetyksi ajaksi näppäimen painalluksen jälkeen valoisuudesta riippuen</translation>
+    </message>
+    <message id="bg-timeout-slider">
+        <source>Backlight timeout</source>
+        <extracomment>Backlight timeout slider name</extracomment>
+        <translation>Taustavalon aikakatkaisu</translation>
+    </message>
+    <message id="bg-brightness-slider">
+        <source>Brightness threshold</source>
+        <extracomment>Backlight brightness threshold slider</extracomment>
+        <translation>Valoisuuden raja-arvo</translation>
+    </message>
+    <message id="orientation-sect-header">
+        <source>Orientation</source>
+        <extracomment>Section header for orientation settings</extracomment>
+        <translation>Näytön suunta</translation>
+    </message>
+    <message id="orientation-force-landscape-sw">
+        <source>Force Landscape</source>
+        <extracomment>Force landsacep switch text</extracomment>
+        <translation>Pakota vaaka-asento</translation>
+    </message>
+    <message id="orientation-force-landscape-desc">
+        <source>Force landscape orientation when keyboard attached</source>
+        <extracomment>Force landsacep switch description</extracomment>
+        <translation>Pakota puhelimen näyttö vaaka-asentoon näppäimistön ollessa liitetty</translation>
+    </message>
+    <message id="repeat-sect-header">
+        <source>Repeat</source>
+        <extracomment>Section header for repeat settings</extracomment>
+        <translation>Näppäinten toisto</translation>
+    </message>
+    <message id="repeat-delay-slider">
+        <source>Repeat start delay</source>
+        <extracomment>Keyboard repeat start delay slider</extracomment>
+        <translation>Toiston aloitusviive</translation>
+    </message>
+    <message id="repeat-rate-slider">
+        <source>Repeat rate</source>
+        <extracomment>Keyboard repeat rate slider</extracomment>
+        <translation>Toiston nopeus</translation>
+    </message>
+    <message id="test-here">
+        <source>Test here</source>
+        <extracomment>Placeholder text for textfield to test repeat settings</extracomment>
+        <translation>Kokeile tässä</translation>
+    </message>
+    <message id="sticky-sect-header">
+        <source>Sticky and locking modifier keys</source>
+        <extracomment>Section header for sticky and locking settings</extracomment>
+        <translation>Pohjaan jäävät ja lukittuvat näppäimet</translation>
+    </message>
+    <message id="sticky-desc">
+        <source>Sticky modifiers will toggle when pressed once and released after pressing any other key. Locking modifier will lock on double-press and released on third. In both modes you can also use them as normal modifier keys</source>
+        <extracomment>Description text for sticky and locking modifier keys</extracomment>
+        <translation>Pohjaan jäävät näppäimet vaihtavat tilaa painamalla niitä kerran, ja vapautuvat painamalla jotain muuta näppäint. Lukittuvat näppäimet jäävät päälle kaksi kertaa peräkkäin painettaessa. Näppäimet toimivat myös normaalisti vaikka jompi kumpi tila olisi valittu.</translation>
+    </message>
+    <message id="sticky-shift">
+        <source>Sticky Shift</source>
+        <translation>Pohjaan jäävä Shift</translation>
+    </message>
+    <message id="locking-shift">
+        <source>Locking Shift</source>
+        <translation>Lukittuva Shift</translation>
+    </message>
+    <message id="sticky-ctrl">
+        <source>Sticky Ctrl</source>
+        <translation>Pohjaan jäävä Ctrl</translation>
+    </message>
+    <message id="locking-ctrl">
+        <source>Locking Ctrl</source>
+        <translation>Lukittuva Ctrl</translation>
+    </message>
+    <message id="sticky-alt">
+        <source>Sticky Alt</source>
+        <translation>Pohjaan jäävä Alt</translation>
+    </message>
+    <message id="locking-alt">
+        <source>Locking Alt</source>
+        <translation>Lukittuva Alt</translation>
+    </message>
+    <message id="sticky-sym">
+        <source>Sticky Sym</source>
+        <translation>Pohjaan jäävä Sym</translation>
+    </message>
+    <message id="locking-sym">
+        <source>Locking Sym</source>
+        <translation>Lukittuva Sym</translation>
+    </message>
+    <message id="active-layout">
+        <source>Active</source>
+        <extracomment>label showing this layout is currently selected</extracomment>
+        <translation>Aktiivinen</translation>
+    </message>
+    <message id="select-layout">
+        <source>Select</source>
+        <extracomment>Button text for button to select this as active LayoutItem</extracomment>
+        <translation>Valitse</translation>
+    </message>
+    <message id="reset-to-defaults">
+        <source>Reset all to defaults</source>
+        <extracomment>Pulldown menu optiion to reset default values</extracomment>
+        <translation>Palauta oletukset</translation>
+    </message>
+    <message id="shortcuts">
+        <source>Shortcuts</source>
+        <extracomment>Main menu selection for shortcuts configurations</extracomment>
+        <translation>Oikopolut</translation>
+    </message>
+    <message id="settings">
+        <source>Settings</source>
+        <extracomment>Main page header</extracomment>
+        <translation>Asetukset</translation>
+    </message>
+    <message id="layout">
+        <source>Layout</source>
+        <extracomment>Main menu selection for layout selection</extracomment>
+        <translation>Näppäinasettelu</translation>
+    </message>
+</context>
+</TS>
